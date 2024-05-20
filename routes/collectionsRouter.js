282a64d91394ee06ba37dbbd330007682245dd2b
Yauheni Kadyrau <7077634@gmail.com>
2024-05-20 14:59:37 +0300

    fix: fix collections optionalField and url

--- routes/collectionsRouter.js
+++ routes/collectionsRouter.js
@@ -7,10 +7,10 @@ const router = new Router()
 const authMiddleware = require('../authMiddleware/authMiddleWare')
 
 
-router.get('/collections', getCollections)
-router.post('/collections', authMiddleware, createCollection)
-router.delete('/collections/:id', authMiddleware, deleteCollection)
-router.put('/collections/:id', authMiddleware, updateCollection)
+router.get('/', getCollections)
+router.post('/', authMiddleware, createCollection)
+router.delete('/:id', authMiddleware, deleteCollection)
+router.put('/:id', authMiddleware, updateCollection)
 
 
 module.exports = router
